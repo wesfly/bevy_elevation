2a09de5afc12f080c12e4590b8494f7bffaf1ec5
wesfly <wesfly@noreply.codeberg.org>
2026-02-18 13:36:33 +0100

    Evenly distributed vertices on sphere and half-implemented chunk logic

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ use bevy::camera_controller::free_camera::{FreeCamera, FreeCameraPlugin};
 use bevy::dev_tools::fps_overlay::FpsOverlayPlugin;
 use bevy::prelude::*;
 
-// #[cfg(debug_assertions)]
-// use avian3d::debug_render::PhysicsDebugPlugin;
+#[cfg(debug_assertions)]
+use avian3d::debug_render::PhysicsDebugPlugin;
 
 fn main() {
     App::new()
@@ -13,8 +13,8 @@ fn main() {
             DefaultPlugins,
             FreeCameraPlugin,
             PhysicsPlugins::default(),
-            // #[cfg(debug_assertions)]
-            // PhysicsDebugPlugin,
+            #[cfg(debug_assertions)]
+            PhysicsDebugPlugin,
         ))
         .add_systems(Startup, setup)
         .add_plugins(FpsOverlayPlugin::default())
@@ -52,64 +52,71 @@ pub fn spawn_chunk(
     materials: &mut ResMut<Assets<StandardMaterial>>,
     normal: Dir3,
 ) {
-    const SIZE: f32 = 0.2;
+    // const SIZE: f32 = 0.2;
     const RADIUS: f32 = 5.0;
+    const CHUNKS: u8 = 9;
 
-    let mut obj_mesh = Mesh::from(
-        Plane3d::default()
-            .mesh()
-            .size(SIZE * 2.0, SIZE * 2.0)
-            .normal(normal)
-            .subdivisions(128),
-    );
+    for i in 0..1 {
+        let chunk_translation = Vec3 {
+            x: normal.x,
+            y: normal.y,
+            z: normal.z,
+        };
 
-    if let bevy::mesh::VertexAttributeValues::Float32x3(positions) = obj_mesh
-        .try_attribute_mut(Mesh::ATTRIBUTE_POSITION)
-        .unwrap()
-    {
-        for pos in positions.iter_mut() {
-            // Positioning the plane to normalize it
-            let position = normal.as_vec3().to_array();
-            pos[0] += SIZE * position[0];
-            pos[1] += SIZE * position[1];
-            pos[2] += SIZE * position[2];
+        let mut earth_mesh_even = Mesh::from(
+            Plane3d::default()
+                .mesh()
+                .size(
+                    2.0, // CHUNKS.isqrt() as f32,
+                    2.0, // CHUNKS.isqrt() as f32,
+                )
+                .normal(normal)
+                .subdivisions(32),
+        )
+        .translated_by(chunk_translation);
 
-            let p = Vec3 {
-                x: pos[0],
-                y: pos[1],
-                z: pos[2],
-            };
+        if let bevy::mesh::VertexAttributeValues::Float32x3(positions) = earth_mesh_even
+            .try_attribute_mut(Mesh::ATTRIBUTE_POSITION)
+            .unwrap()
+        {
+            for pos in positions.iter_mut() {
+                let p = Vec3 {
+                    x: pos[0],
+                    y: pos[1],
+                    z: pos[2],
+                };
 
-            // Even spacing of vertices on sphere
-            let x = p.x
-                * (1.0 - (p.y.powi(2) + p.z.powi(2)) / 2.0 + (p.y.powi(2) * p.z.powi(2) / 3.0))
-                    .sqrt();
-            let y = p.y
-                * (1.0 - (p.z.powi(2) + p.x.powi(2)) / 2.0 + (p.z.powi(2) * p.x.powi(2) / 3.0))
-                    .sqrt();
-            let z = p.z
-                * (1.0 - (p.x.powi(2) + p.y.powi(2)) / 2.0 + (p.x.powi(2) * p.y.powi(2) / 3.0))
-                    .sqrt();
-            let even_spaced_pos = Vec3::new(x, y, z);
+                // Even spacing of vertices on sphere
+                let x = p.x
+                    * (1.0 - (p.y.powi(2) + p.z.powi(2)) / 2.0 + (p.y.powi(2) * p.z.powi(2) / 3.0))
+                        .sqrt();
+                let y = p.y
+                    * (1.0 - (p.z.powi(2) + p.x.powi(2)) / 2.0 + (p.z.powi(2) * p.x.powi(2) / 3.0))
+                        .sqrt();
+                let z = p.z
+                    * (1.0 - (p.x.powi(2) + p.y.powi(2)) / 2.0 + (p.x.powi(2) * p.y.powi(2) / 3.0))
+                        .sqrt();
+                let even_spaced_pos = Vec3::new(x, y, z);
 
-            *pos = (even_spaced_pos.normalize() * RADIUS).to_array();
+                *pos = (even_spaced_pos).to_array();
+            }
         }
-    }
 
-    obj_mesh.compute_normals();
-    commands.spawn((
-        Collider::trimesh_from_mesh(&obj_mesh).unwrap(),
-        Mesh3d(meshes.add(obj_mesh)),
-        RigidBody::Static,
-        MeshMaterial3d(materials.add(StandardMaterial {
-            base_color: bevy::color::palettes::css::GREEN.into(),
-            perceptual_roughness: 1.0,
-            ..Default::default()
-        })),
-        Transform::from_translation(Vec3 {
-            x: 0.0,
-            y: -0.5 * RADIUS,
-            z: 0.0,
-        }),
-    ));
+        earth_mesh_even.compute_normals();
+        commands.spawn((
+            Collider::trimesh_from_mesh(&earth_mesh_even).unwrap(),
+            Mesh3d(meshes.add(earth_mesh_even)),
+            RigidBody::Static,
+            MeshMaterial3d(materials.add(StandardMaterial {
+                base_color: bevy::color::palettes::css::GREEN.into(),
+                perceptual_roughness: 1.0,
+                ..Default::default()
+            })),
+            Transform::from_translation(Vec3 {
+                x: 0.0,
+                y: -0.5 * RADIUS,
+                z: 0.0,
+            }),
+        ));
+    }
 }
